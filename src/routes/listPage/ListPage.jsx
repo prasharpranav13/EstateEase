7764765eaeb7d258a5572643bf0cdd9e15b46226
Pranav <pranavparasar321@gmail.com>
2024-06-06 16:03:10 +0530

    added dynamic  map

--- src/routes/listPage/ListPage.jsx
+++ src/routes/listPage/ListPage.jsx
@@ -2,6 +2,7 @@ import "./listPage.scss";
 import { listData } from "../../lib/dummyData";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
 const ListPage = () => {
   const data = listData;
   return (
@@ -14,7 +15,9 @@ const ListPage = () => {
           ))}
         </div>
       </div>
-      <div className="mapContainer">map</div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 };
